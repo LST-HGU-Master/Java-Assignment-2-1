cdead2e0217e48b0148dd54840d19f6b2d9ed1cb
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-04-18 16:18:07 +0900

    Update Prog21Test.java
    lineseparator adjustment

--- src/test/java/Prog21Test.java
+++ src/test/java/Prog21Test.java
@@ -15,7 +15,7 @@ class Prog21Test {
         Prog21.main(new String[]{"10", "19"});
 
         // assertion
-        String[] prints = bos.toString().split("\n");
+        String[] prints = bos.toString().split(System.lineSeparator());
         assertEquals("x=10,y=19とすると、", prints[0]); //改行でsplitしてるので改行コードは不要
 
         // undo the binding in System
@@ -33,7 +33,7 @@ class Prog21Test {
         Prog21.main(new String[]{"10", "19"});
 
         // assertion
-        String[] prints = bos.toString().split("\n");
+        String[] prints = bos.toString().split(System.lineSeparator());
         assertEquals("x+y = 29", prints[1]); //改行でsplitしてるので改行コードは不要
 
         // undo the binding in System
@@ -51,7 +51,7 @@ class Prog21Test {
         Prog21.main(new String[]{"10", "19"});
 
         // assertion
-        String[] prints = bos.toString().split("\n");
+        String[] prints = bos.toString().split(System.lineSeparator());
         assertEquals("y/x = 1.9", prints[2]);
 
         // undo the binding in System
